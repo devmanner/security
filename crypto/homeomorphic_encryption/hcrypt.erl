0adaa3e7e26e33536e54adff97e41716f96c52c1
Tomas Mannerstedt <tmannerstedt@hotmail.com>
2024-05-10 10:38:17 +0200

    Some changes...

--- crypto/homeomorphic_encryption/hcrypt.erl
+++ crypto/homeomorphic_encryption/hcrypt.erl
@@ -1,7 +1,12 @@
 -module(hcrypt).
 -export([test/0]).
 -export([doit/2]).
--export([pow2/1, pow3/1]).
+
+-export([pow2/1]).
+-export([mult/1]).
+-export([sum/1]).
+-export([sum2/1]).
+-export([divide/1]).
 
 % Example of Homeomorphic encryption.
 % doit/1 does all the things. pow2 is the function to be calculated on an insecure system and only gets the input in 
@@ -23,30 +28,67 @@ doit(Input, Fun) ->
     {RSAPublic, RSAPrivate} = crypto:generate_key(rsa, {KeyLen, PubExp}),
     [_, Mod] = RSAPublic,
 
-    Cipher = crypto:public_encrypt(rsa, pack(Input, KeyLen), RSAPublic, RSAOpt),
+    Cipher = lists:map(fun(X) -> crypto:public_encrypt(rsa, pack(X, KeyLen), RSAPublic, RSAOpt) end, Input),
 
     R = calc(Fun, Cipher, Mod),
     
     Plain = crypto:private_decrypt(rsa, R, RSAPrivate, RSAOpt),
     unpack(Plain).
 
-
 calc(Fun, Blind, M) ->
-    Decoded = binary:decode_unsigned(Blind),
+    Decoded = lists:map(fun(X) -> binary:decode_unsigned(X) end, Blind),
     Mod = binary:decode_unsigned(M),
-    binary:encode_unsigned(erlang:apply(?MODULE, Fun, [Decoded]) rem Mod).
+    R = erlang:apply(?MODULE, Fun, [Decoded]),
+    binary:encode_unsigned(R rem Mod).
 
-
-pow2(X) ->
-    X*X.
-pow3(X) ->
-    X*X*X.
+% Remember that the operands here may be very large... so don't do too complex operations.
+pow2([X]) ->
+    X * X.
+mult([X, Y]) ->
+    X * Y.
+divide([X, Y]) ->
+    X div Y.
+sum2([X, Y]) ->
+    X + Y.
+sum(L) ->
+    lists:foldl(fun(X, Acc) -> X + Acc end, 0, L).
 
 test() ->
     0 = unpack(pack(0, 2048)),
     101 = unpack(pack(101, 2048)),
     1234567 = unpack(pack(1234567, 2048)),
-    4 = doit(2, pow2),
+
+    lists:foreach(fun(_) -> 
+        A = rand:uniform(100000) + 1,
+        B = rand:uniform(100000) + 1, % Avoid divide by 0
+        io:format("A: ~p~nB:~p~n~n", [A, B]),
+        A2 = pow2([A]),
+        A2 = doit([A], pow2),
+
+        AB = mult([A, B]),
+        AB = mult([B, A]),
+        AB = doit([A, B], mult),
+        AB = doit([B, A], mult),
+
+        ApB = sum2([A, B]),
+        ApB = sum2([B, A]),
+        ApB = doit([A, B], sum2),
+        ApB = doit([B, A], sum2),
+        
+
+
+        AdivB = divide([A, B]),
+        AdivB2 = doit([A, B], divide),
+        io:format("Exp result: ~p Result: ~p~n", [AdivB, AdivB2]),
+        AdivB = AdivB2
+    end, lists:seq(1,10)),
+
+    2 = doit([1, 1], sum2),
+
+    10 = doit([3, 7], sum2),
+    55 = doit(lists:seq(1, 10), sum),
+
+    10 = doit([100, 10], divide),
 
     ok.
 
