4115ae217f89dc242be99e5d67c6ccc68c7cdbcf
Tomas Mannerstedt <tmannerstedt@hotmail.com>
2024-05-24 15:56:55 +0200

    Not running as root anymore. User password is given at build time. Note that hash is saved locally.

---
+++ kali/mkpasswd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Variables
+username="$1"
+
+echo -n Password: 
+read -s password
+echo
+
+# Generate password hash
+hash=$(/opt/homebrew/bin/openssl passwd -6 "$password")
+
+# Entry fields
+lastchange=$(date +%s)  # Number of days since 1970-01-01
+minimum=0
+maximum=99999
+warn=7
+inactive=""
+expire=""
+reserved=""
+
+# Combine fields into the shadow file format
+new_entry="$username:$hash:$lastchange:$minimum:$maximum:$warn:$inactive:$expire:$reserved"
+
+echo "$new_entry"
